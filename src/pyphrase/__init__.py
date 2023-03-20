1ea932156e844c7558a3cfe45dac1a642dbb0cf8
Henrik Kühnemann <hk@yellownape.se>
2023-03-20 13:36:16 +0100

    bump version 0.1.0 -> 0.2.0

--- src/pyphrase/__init__.py
+++ src/pyphrase/__init__.py
@@ -1,6 +1,6 @@
 from .asynchron.client import AsyncPhraseTMSClient, async_get_phrase_tms_token
 from .synchron.client import SyncPhraseTMSClient, sync_get_phrase_tms_token
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     AsyncPhraseTMSClient,
