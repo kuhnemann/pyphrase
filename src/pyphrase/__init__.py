1507b27ec7f75dfa6067b0815e5f791e227ce862
Henrik Kühnemann <henrik.kuhnemann@translator-scandinavia.se>
2024-02-11 17:10:19 +0100

    added missing version

--- src/pyphrase/__init__.py
+++ src/pyphrase/__init__.py
@@ -1,5 +1,6 @@
 from .asynchron.client import AsyncPhraseTMSClient, async_get_phrase_tms_token
 from .synchron.client import SyncPhraseTMSClient, sync_get_phrase_tms_token
+__version__ = "0.2.2"
 
 __all__ = [
     AsyncPhraseTMSClient,
